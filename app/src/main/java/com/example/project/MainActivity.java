d6ecb43ea604df5440c2fa78f224cb0e715c6c12
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-26 22:41:53 +0200

    added button that launches second activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        this.intent = new Intent(MainActivity.this, SecondActivity.class);
     }
 
+    public void launchSecondActivity(View view) {
+        intent.setAction(Intent.ACTION_SEND);
+    }
 }
