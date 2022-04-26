6bd6f4b29908e9e1140b87b0b4d9b288936be4b1
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-26 23:44:27 +0200

    write text to shared pref. on save, close SecondActivity and update textView in MainActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.project;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,6 +13,8 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
+    private SharedPreferences preferences;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        this.preferences = getSharedPreferences("com.example.project.data", Context.MODE_PRIVATE);
+        this.resultText = findViewById(R.id.textView);
+
         this.intent = new Intent(MainActivity.this, SecondActivity.class);
         intent.setAction(Intent.ACTION_SEND);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String text = preferences.getString("text", "Shared Preferences");
+
+        resultText.setText(text);
+
     }
 
     public void launchSecondActivity(View view) {
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -7,27 +7,23 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
 public class SecondActivity extends AppCompatActivity {
 
     private SharedPreferences preferences;
     private EditText editText;
-    private final String SHARED_PREFERENCES_NAME = "com.example.project.data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        this.preferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        this.preferences = getSharedPreferences("com.example.project.data", Context.MODE_PRIVATE);
         this.editText = findViewById(R.id.editText);
     }
 
     public void saveToSharedPreferences(View view) {
-
         if (editText.getText().toString().equals("")) {
             Log.d("Preferences", "Empty string");
             return;
